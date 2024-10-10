677ff061edf62a136951ca19a246e51b215019f4
Abdou-Raouf ATARMLA <achilleatarmla@gmail.com>
2024-10-10 19:57:39 +0000

    update: update string helpers

--- src/helpers/string.ts
+++ src/helpers/string.ts
@@ -13,3 +13,19 @@ export const slugify = (text: string): string => {
 export const escapeRegex = (text: string): string => {
   return text.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&');
 };
+
+export const parseSortParam = (sortParam: string): Record<string, number> => {
+  const sortFields = sortParam.split(',');
+  const sortObj: Record<string, number> = {};
+
+  sortFields.forEach((field) => {
+    if (field.startsWith('-')) {
+      const fieldName = field.substring(1);
+      sortObj[fieldName] = -1;
+    } else {
+      sortObj[field] = 1;
+    }
+  });
+
+  return sortObj;
+};
