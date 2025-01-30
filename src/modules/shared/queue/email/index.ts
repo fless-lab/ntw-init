835c773fd69a534ece55e60310988e374dddec86
Abdou-Raouf ATARMLA <achilleatarmla@gmail.com>
2025-01-30 19:25:19 +0000

    feat: export and initialized email queue manager

--- src/modules/shared/queue/email/index.ts
+++ src/modules/shared/queue/email/index.ts
@@ -1,2 +1,7 @@
+import { EmailQueueManager } from './email.queue';
+
+// Initialize the queue manager at startup
+EmailQueueManager.getInstance();
+
 export { emailQueue } from './email.queue';
 export { default as EmailQueueService } from './email.queue.service';
